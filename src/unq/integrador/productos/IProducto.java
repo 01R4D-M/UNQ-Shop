b66f3dad2dc9e6621a09ea82151166888cb3b312
Ruben Dario Monteros <monterosrubendario@gmail.com>
2026-06-29 16:33:19 -0300

    agrego los getters de peso y precio final a la interfaz del producto

--- src/unq/integrador/productos/IProducto.java
+++ src/unq/integrador/productos/IProducto.java
@@ -4,4 +4,6 @@ public interface IProducto {
 	public String getNombre();
 	public String getDescripcion();
 	public double getPrecioBase();
+	public double getPeso();
+	public double getPrecioFinal();
 }

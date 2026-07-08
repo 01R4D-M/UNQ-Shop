11e6963a5d914ac351d8453d7cd082fc08910231
Diego Arroyo <diegoramiroarroyo@gmail.com>
2026-07-07 22:35:23 -0300

    Metodo Test

--- test/unq/integrador/pago/MetodoDePagoTest.java
+++ test/unq/integrador/pago/MetodoDePagoTest.java
@@ -4,43 +4,26 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MetodoDePagoTest {
 
+    private MetodoDePago metodoDePago;
+
+    @BeforeEach
+    void setUp() {
+        metodoDePago = mock(MetodoDePago.class);
+    }
+
     @Test
     void testValidarPagoEjecutaTodosLosPasos() {
-        MetodoDePago metodoDePago = new MetodoDePago() {
-            @Override
-            public void validarDatos() {
-            }
-
-            @Override
-            public void reservarFondos() {
-            }
-
-            @Override
-            public void ejecutarTransaccion() {
-            }
-
-            @Override
-            public void notificarResultado() {
-            }
-
-            @Override
-            public void reembolsarPago(double monto) {
-            }
-        };
-
         assertDoesNotThrow(() -> metodoDePago.validarPago(1000.0));
     }
 
     @Test
     void testReembolsarPagoDelegadoAlMetodoConcreto() {
-        MetodoDePago metodoDePago = mock(MetodoDePago.class);
-
         metodoDePago.reembolsarPago(250.0);
-
         verify(metodoDePago).reembolsarPago(250.0);
     }
 }

415d503e4341c94f2965399f40f91e573bdacd9f
Diego Arroyo <diegoramiroarroyo@gmail.com>
2026-07-09 10:29:06 -0300

    DirTest

--- test/unq/integrador/envio/DireccionTest.java
+++ test/unq/integrador/envio/DireccionTest.java
@@ -19,7 +19,8 @@ public class DireccionTest {
 
     @Test
     void testGetters() {
-        assertEquals("Calle Falsa 123", direccion.getCalle());
+        assertEquals("Calle Falsa", direccion.getCalle());
+        assertEquals("123", direccion.getNumero());
         assertEquals("Springfield", direccion.getCiudad());
         assertEquals("12345", direccion.getCodigoPostal());
         assertEquals("Buenos Aires", direccion.getProvincia());
